eaa86aac506cf0087bed509e177451683dab88db
pvictortm <100025389+pvictortm@users.noreply.github.com>
2023-06-20 22:56:07 -0300

    Add files via upload

---
+++ VariaveisNumericasInteiras.java
@@ -0,0 +1,25 @@
+public class VariaveisNumericasInteiras {
+
+    public static void main(String[] args){
+        int idadePaulo;
+        idadePaulo =  28;
+
+        System.out.println("A idade do Paulo é " + idadePaulo + " anos!");
+
+        int idadeAna = 25;
+        System.out.println(idadeAna);
+
+        // tipos de dados para trabalhar numeros inteiros
+        // byte - 8 bits = -128 ate 127
+        // short - 16 bits =  -32768 ate 32767
+        // int - 32 bits
+        // long - 64 bits
+
+        byte testaByte = 127;
+
+        int testaExpressao = 10 * 5 + 50 + 24/2;
+
+        System.out.println("O resultado da expressão é: " + testaExpressao);
+    }
+}
+
