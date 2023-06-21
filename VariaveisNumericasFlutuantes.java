b80d86161e97d9b75934a1c5f18433a3d7755db5
pvictortm <100025389+pvictortm@users.noreply.github.com>
2023-06-20 23:10:21 -0300

    Add files via upload

---
+++ VariaveisNumericasFlutuantes.java
@@ -0,0 +1,28 @@
+public class VariaveisNumericasFlutuantes {
+
+    public static void main(String[] args){
+
+        int idadePedro = 28;
+
+        //int salario = 1156.50;
+        //float - 32 bits
+        //double - 64 bits
+
+        double nota1 = 10;
+        double nota2 = 9.5;
+
+        System.out.println("A minha primeira nota foi: " + nota1);
+        System.out.println("A minha segunda nota foi: " +nota2);
+
+        double mediaNotas = (nota1+ nota2)/2;
+
+        System.out.println("A minha média foi de: " + mediaNotas);
+
+        double resultadoDivisao = 15/2.0;
+
+        //int resultado = 7.5;
+
+        System.out.println(resultadoDivisao);
+    }
+}
+
